0333cab6d7e782b4b5ce0ecd7052a74922423566
Jamey <jybryce4@gmail.com>
2020-09-14 01:45:20 -0500

    added current-image-save-on-reload functionality; removed image-loop; can click on image to change

--- js/bg.js
+++ js/bg.js
@@ -1,25 +1,50 @@
 const bg = document.getElementById("header");
 
 // background image URLs
-imageURLs = ['img/bg1.jpg', 'img/bg2.jpg', 'img/bg3.jpg', 'img/bg4.jpg', 'img/bg5.jpg'];
+imageURLs = ['img/bg1.jpg', 'img/bg2.jpg', 'img/bg3.jpg', 'img/bg4.jpg', 'img/bg5.jpg', 'img/bg6.jpg'];
+
+// needed for saving the current image
+let imgUrl, imgIndex;
+
+// save image state
+let currentImage = localStorage.getItem("BG-IMG");
+let currentImageID = localStorage.getItem("BG-IMG-ID");
+
+// check if image is there
+if (currentImage) {
+    imgUrl = currentImage;
+    imgIndex = currentImageID;
+    loadImage(imgUrl); // load image if it is saved
+} else { 
+    // set default image
+    imgUrl = imageURLs[0];
+    imgIndex = 0;
+}
+
+// load the current image into UI
+function loadImage(img) {
+    bg.style.backgroundImage = `url(${img})`;
+}
 
 // background image changer
-let imgIndex = 0;
-function background() {
+bg.addEventListener("click", event => {
     // reset the index as needed
     if (imgIndex >= imageURLs.length) {
         imgIndex = 0;
     }
 
     // set the image
-    bg.style.backgroundImage = `url(${imageURLs[imgIndex++]})`;
+    bg.style.backgroundImage = `url(${imageURLs[imgIndex]})`;
 
-    // time the changes
-    setTimeout(background, 60000);
-}
+    // add current image to localStorage
+    localStorage.setItem("BG-IMG", imageURLs[imgIndex]);
+    localStorage.setItem("BG-IMG-ID", imgIndex);
 
-// run it
-background();
+    // increment index
+    imgIndex++;
+    
+});
+
 
 
 /*
